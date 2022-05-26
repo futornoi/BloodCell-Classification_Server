a0aa4d6ad293ba6b8d293cf2fd1cc3063f7d9969
maksim <futornoi.maksim@gmail.com>
2022-05-26 17:25:07 +0300

    add prod build v2

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = 3001;
+const staticPath = path_1.default.join(__dirname, '../ModelData', 'model_data');
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.static(staticPath));
+app.get('/model_metadata', (req, res) => {
+    res.sendFile(staticPath + '/model.json');
+});
+app.listen(PORT, () => {
+    console.log(`Server starting on ${PORT} port`);
+});
