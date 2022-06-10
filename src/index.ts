296c103850f37d78674fb45265aebaaa52d74843
maksim <futornoi.maksim@gmail.com>
2022-06-10 20:33:12 +0300

    rename model directory v3

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const {PORT = 3001} = process.env;
-const staticPath = path.join(__dirname, '../ModelData_train', 'model_data');
+const staticPath = path.join(__dirname, '../ModelData', 'model_data');
 
 app.use(cors());
 app.use(express.json());
